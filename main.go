84f844e14b3e76e768088bf617339f21cde4a259
toukii <toukii@yahoo.com>
2016-11-02 22:02:04 +0800

    auto

--- main.go
+++ main.go
@@ -3,13 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/go-macaron/macaron"
-	// "github.com/toukii/goutils"
+	"github.com/toukii/goutils"
+	"github.com/toukii/wrdGM/util"
 	"html/template"
+	"strconv"
+	"strings"
 )
 
 var (
-	seq = "AaeHYtVmuIOqKzXl"
+	seq        = "AaeHYtVmuIOqKzXl"
+	letters    = make([]string, 16)
+	lettersMap = make(map[string]string)
 )
+
+func init() {
+	for i := 0; i < 16; i++ {
+		letters[i] = string(seq[i])
+		lettersMap[fmt.Sprintf("%d", i)] = letters[i]
+	}
+}
 
 func main() {
 	m := macaron.Classic()
@@ -22,7 +34,7 @@ func main() {
 	}))
 	m.Group("/", func() {
 		m.Get("", word)
-		m.Post("", chck)
+		m.Post("word", chck)
 		m.Combo("tube").Get(tube)
 	})
 	m.Run()
@@ -36,18 +48,29 @@ func word(ctx *macaron.Context) {
 	fmt.Println("word")
 	data := make(map[string]interface{})
 	data["UserName"] = "shaalx"
-	letters := make([]string, 16)
-	for i := 0; i < 16; i++ {
-		letters[i] = string(seq[i])
-	}
 	data["seq"] = letters
 	ctx.HTML(200, "word", data)
 }
 
 func chck(ctx *macaron.Context) {
-	poss := ctx.Params("poss")
-	fmt.Println(poss)
-	ctx.JSON()
+	possRaw := ctx.Query("poss")
+	poss := strings.Split(possRaw, ",")
+	words := ""
+	for i := 0; i < len(poss)-1; i++ {
+		it := poss[i]
+		pos, err := strconv.ParseInt(it, 10, 16)
+		if goutils.CheckErr(err) {
+			ctx.JSON(302, err.Error())
+		}
+		if i > 0 {
+			if !util.ChckPos4(i-1, i) {
+				ctx.JSON(303, "pos error")
+			}
+		}
+		words += letters[pos]
+	}
+	fmt.Println(words)
+	ctx.JSON(200, words)
 }
 
 func mod4(i int) bool {
